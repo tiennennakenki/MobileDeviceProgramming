a49bbb9bee8f43ad6c90b3bb7fb01b62bf62d99b
Tien <tiennennakenki@gmail.com>
2023-05-20 15:26:50 +0700

    update interface login and register

--- mobile_device/lib/login/page_login.dart
+++ mobile_device/lib/login/page_login.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_device/register/page_register.dart';
 
 import 'laptop_connect.dart';
-import 'login_auth_service.dart';
+import 'auth_service.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -19,45 +20,73 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Đăng nhập'),
-      ),
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                ),
-                validator: (val) =>
-                val!.isEmpty ? 'Vui lòng nhập email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
+              Image.asset(
+                'assets/logo.png',
+                height: 150,
               ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Mật khẩu',
+              SizedBox(height: 40.0),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      validator: (val) =>
+                      val!.isEmpty ? 'Vui lòng nhập email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Mật khẩu',
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (val) =>
+                      val!.length < 6 ? 'Mật khẩu phải có ít nhất 6 ký tự' : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                  ],
                 ),
-                obscureText: true,
-                validator: (val) => val!.length < 6
-                    ? 'Mật khẩu phải có ít nhất 6 ký tự'
-                    : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
               ),
               SizedBox(height: 20.0),
               ElevatedButton(
                 onPressed: () async {
                   if (_formKey.currentState!.validate()) {
                     dynamic result = await _auth.signInWithEmailAndPassword(
-                        email, password);
+                      email,
+                      password,
+                    );
                     if (result == null) {
                       setState(() =>
                       error = 'Đăng nhập không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.');
@@ -73,13 +102,38 @@ class _LoginPageState extends State<LoginPage> {
                 },
                 child: Text(
                   'Đăng nhập',
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(fontSize: 16),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                 ),
               ),
-              SizedBox(height: 12.0),
+              SizedBox(height: 10.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RegisterPage(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Đăng ký tài khoản',
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+              SizedBox(height: 10.0),
               Text(
                 error,
-                style: TextStyle(color: Colors.red, fontSize: 14.0),
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14.0,
+                ),
               ),
             ],
           ),
@@ -88,3 +142,4 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+
---
+++ mobile_device/lib/register/page_register.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_device/login/auth_service.dart';
+
+import '../login/page_login.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+  String confirmPassword = '';
+  String error = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset(
+                'assets/logo.png',
+                height: 150,
+              ),
+              SizedBox(height: 40.0),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      validator: (val) =>
+                      val!.isEmpty ? 'Vui lòng nhập email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Mật khẩu',
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (val) =>
+                      val!.length < 6 ? 'Mật khẩu phải có ít nhất 6 ký tự' : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Xác nhận mật khẩu',
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (val) =>
+                      val != password ? 'Mật khẩu không khớp' : null,
+                      onChanged: (val) {
+                        setState(() => confirmPassword = val);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    dynamic result = await _auth.registerWithEmailAndPassword(
+                        email, password);
+                    if (result == null) {
+                      setState(() => error =
+                      'Đăng ký không thành công. Vui lòng kiểm tra lại thông tin đăng ký.');
+                    } else {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginPage(),
+                        ),
+                      );
+                    }
+                  }
+                },
+                child: Text(
+                  'Đăng ký',
+                  style: TextStyle(fontSize: 16),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10.0),
+              TextButton(
+                onPressed: () {
+                  // Xử lý chuyển sang trang đăng nhập
+                  print('Chuyển sang trang đăng nhập');
+                },
+                child: Text(
+                  'Đăng nhập',
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
